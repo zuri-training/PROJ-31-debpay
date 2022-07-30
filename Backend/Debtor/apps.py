7ada6a5f5c0902121c870ef3413ae1204cf9118f
gphics <97003589+gphics@users.noreply.github.com>
2022-07-29 22:28:29 -0700

    Created django Project

---
+++ Backend/Debtor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DebtorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Debtor'
---
+++ Backend/Debtor/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .forms import*
+from .models import*
+# Create your views here.
