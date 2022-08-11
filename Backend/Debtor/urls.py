3636af2e789b608e4912c655f452402010673344
Greenie00 <georgegbalinyo@gmail.com>
2022-08-11 09:36:52 +0100

    Integration of frontend into backend(incomplete)

--- Backend/Debtor/urls.py
+++ Backend/Debtor/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('dashboard/', views.school_dashboard, name='dashboard'),
     path('dashboard', views.dashboard, name='dashboard'),
     path('register', views.School_Register, name='register'),
+    path('privacy&policy', views.privacy, name='p&p'),
     path('logout', views.School_Logout, name='logout'),
     path('login', views.School_Login, name='login'),
     path('post_create', views.CreatePost, name='post_create'),
--- Backend/Debtor/views.py
+++ Backend/Debtor/views.py
@@ -86,6 +86,10 @@ def School_Logout(request):
 def land(request):
     return render(request, 'Debtor/home.html')
 
+#Privacy & Policy page view
+def privacy(request):
+    return render(request, 'Privacypolicy.html')
+
 #School login view
 def School_Login(request):
     if request.method == 'POST':
