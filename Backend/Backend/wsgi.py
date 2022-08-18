d893c06b3eccc4eeb4cca1c8f1e0d34359fbe493
Joseph Ibochi <josephibochi@gmail.com>
2022-08-18 04:21:22 +0100

    new

--- Backend/Backend/wsgi.py
+++ Backend/Backend/wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = get_wsgi_application()
